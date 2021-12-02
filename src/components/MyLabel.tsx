c60b5b4deaf0a47891747da8bf057eed82525c2f
DESKTOP-H976RTI\jacob <jacoboramirezrivera6@gmail.com>
2021-12-02 20:36:57 +0100

    first commit

---
+++ src/components/MyLabel.tsx
@@ -0,0 +1,36 @@
+import './MyLabel.css';
+
+export interface Props {
+    
+     /**
+     * Mensaje a mostrar en la etiqueta
+     */
+    label: string;
+
+     /**
+     * Tamaño del componente 
+     */
+    size?: 'normal'|'h1'|'h2'|'h3';
+
+    /**
+     * Capitalizar Texto 
+     */
+     allCaps?: boolean;
+
+     /**
+     * Color del texto
+     */
+      color?: 'primary' | 'secondary' | 'tertiary';
+
+    /**
+     * Color del texto
+     */
+    fontColor?: string;     
+}
+
+export const MyLabel = ( {label='No Label', size = 'normal', allCaps = false, color='primary', fontColor}: Props ) => {
+    if(allCaps) label = label.toUpperCase();
+    return (
+        <span style={{color: fontColor}} className={ `label ${size} text-${color}` } >{label}</span>
+    )
+}
